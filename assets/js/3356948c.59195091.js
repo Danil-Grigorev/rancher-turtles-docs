ce97a40c48b60ec642d6b46fd11d7bdd909235d4
Danil-Grigorev <danil.grigorev@suse.com>
2023-09-26 17:56:22 +0000

    Deploying to gh-pages from @ Danil-Grigorev/rancher-turtles-docs@553c43be6e843d9ed2787c638342bf04f0e1828f 🚀

---
+++ assets/js/3356948c.59195091.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmysite=self.webpackChunkmysite||[]).push([[502],{660:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/capi","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
